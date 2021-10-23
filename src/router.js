92923ef2430bec4b8064ceff4059311203735021
Keif Tan <keif_tan@hotmail.com>
2021-10-23 17:46:19 +0800

    added signout, clear all fields, more nrics

--- src/router.js
+++ src/router.js
@@ -7,8 +7,8 @@ import Landing from "./views/Landing.vue";
 import Login from "./views/Login.vue";
 import Register from "./views/Register.vue";
 import Profile from "./views/Profile.vue";
-import first-session from "./views/components/Hero.vue";
-import subsequent-session from "./views/components/Subsequent.vue";
+// import first-session from "./views/components/Hero.vue";
+// import subsequent-session from "./views/components/Subsequent.vue";
 import Sub_show from "./views/Sub_show.vue";
 
 
