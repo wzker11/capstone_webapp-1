44432cbf63789fa7bdfc060b2f5666d162299407
sigridqqq <sigrid.qiu.shichao@gmail.com>
2021-10-28 15:58:33 +0800

    Standardise Code Indentation

--- src/views/ForgetPassword.vue
+++ src/views/ForgetPassword.vue
@@ -11,41 +11,29 @@
             <span></span>
         </div>
         <div class="vue-tempalte">
+            <div class="vue-tempalte">
+                <div class="container pt-lg-md">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-5">
+                            <card type="secondary" shadow header-classes="bg-white pb-5" body-classes="px-lg-5 py-lg-5" class="border-0">
+                                <form @submit.prevent="forgetPassword">
+                                    <h6 class="text-center">Send a password reset email</h6>
+                                    <br>
+                                    <div class="form-group">
+                                        <base-input alternative class="mb-3" placeholder="Email" type="email" addon-left-icon="ni ni-email-83" v-model="user.email"></base-input>
+                                    </div>
 
-        <div class="vue-tempalte">
-            <div class="container pt-lg-md">
-                <div class="row justify-content-center">
-                    <div class="col-lg-5">
-                        <card type="secondary" shadow
-                                    header-classes="bg-white pb-5"
-                                    body-classes="px-lg-5 py-lg-5"
-                                    class="border-0">
-        <form @submit.prevent="forgetPassword">
-            <h6 class="text-center">Send a password reset email</h6>
-            <br>
-            <div class="form-group">
-                                    <base-input alternative class="mb-3"
-                                                            placeholder="Email"
-                                                            type="email"
-                                                            addon-left-icon="ni ni-email-83"
-                                                            v-model="user.email"></base-input>
-                                </div>
-
-            <!-- <div class="form-group">
-                <label>Email</label>
-                <input type="email" class="form-control form-control-lg" v-model="user.email" />
-            </div> -->
-            <div class="row justify-content-center">
-            
-            <button type="submit" class="btn btn-dark btn-lg btn-block">Reset password</button></div>
-            <br>
-            <div class="row justify-content-center"><router-link :to="{name: 'login'}">Sign in</router-link></div>
-        </form>
-        </card>
+                                    <div class="row justify-content-center">
+                                        <button type="submit" class="btn btn-dark btn-lg btn-block">Reset password</button>
+                                    </div>
+                                    <br>
+                                    <div class="row justify-content-center"><router-link :to="{name: 'login'}">Sign in</router-link></div>
+                                </form>
+                            </card>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
     </section>
 </template>
